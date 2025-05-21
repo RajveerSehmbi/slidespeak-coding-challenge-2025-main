add718898c8047f834aa341e7cf68a1df5f86808
Rajveer Sehmbi <rs218@ic.ac.uk>
2025-05-21 23:21:28 +0100

    refactor: remove redundant component

--- frontend/src/components/ConfirmUploadStep/index.tsx
+++ frontend/src/components/ConfirmUploadStep/index.tsx
@@ -2,7 +2,6 @@
 import { FC } from 'react'
 import { UploadedFile } from '@/types/UploadedFile'
 import { DoubleButton } from '../DoubleButton'
-import { ConversionList } from '../ConversionList'
 import { FileHeader } from '../FileHeader'
 
 type ConfirmUploadStepProps = {
@@ -16,7 +15,21 @@ export const ConfirmUploadStep: FC< ConfirmUploadStepProps > = ({ file, onReset,
   return (
     <div data-testid="confirm-upload-step">
       <FileHeader file={file} />
-      <ConversionList />
+      <label className='group flex cursor-pointer gap-2 rounded-xl border-2 border-blue-200 bg-blue-25 p-4'>
+        <div>
+          <div className='grid size-4 place-items-center rounded-full border border-blue-600'>
+            <div className='h-2 w-2 rounded-full bg-blue-600 transition-opacity'></div>
+          </div>
+        </div>
+        <div className='flex flex-col gap-0.5'>
+          <span className='text-sm leading-4 text-blue-800'>
+            Convert to PDF
+          </span>
+          <span className='text-sm text-blue-700'>
+            Best quality, retains images and other assets.
+          </span>
+        </div>
+      </label>
       <DoubleButton 
         leftText={'Cancel'}
         rightText={'Convert'}
